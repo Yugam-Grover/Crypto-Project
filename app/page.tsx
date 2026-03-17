0e66be9f08a28151a6eda3b13ee4da11ba9726fc
Yugam Grover <yugamgrover13@gmail.com>
2026-03-17 19:32:26 +0530

    navigation fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
 'use client'
 import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { usePathname } from 'next/navigation'
-import { cn } from '@/lib/utils'
 const Homepage = () => {
-  const pathname = usePathname();
   return (
-    <header>
-        <div className='main-container inner'>
-          <Link href='/'>
-          <Image loading='eager' src='/Bitstream-logo.png' alt='Bitstream Logo' width={170} height={90}/>      
-           </Link>
-          <nav>
-            <Link href='/' className={cn(
-              'nav-link', {
-              'isactive': pathname==='/',
-               'ishome': true} )}>Home</Link>
-            <p>Search Modal</p>
-            <Link href='/coins' className={cn('nav-link', {'isactive': pathname==='/coins'})}>All Coins</Link>
-          </nav>
-    </div>
-    </header>
+  <div>Homepage</div>
   )
 }
 
